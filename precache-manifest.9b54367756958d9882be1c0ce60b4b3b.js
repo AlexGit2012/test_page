edc5cf46b07086aef4f0ca87d505157595e54ac4
AlexGit2012 <a.karavaychik@gmail.com>
2020-09-16 13:46:06 +0300

    test commit

---
+++ precache-manifest.9b54367756958d9882be1c0ce60b4b3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff990a828df3220048daa34c3a1f137f",
+    "url": "/test_page/index.html"
+  },
+  {
+    "revision": "96a4a8b0b7406de00e23",
+    "url": "/test_page/static/css/main.58a74033.chunk.css"
+  },
+  {
+    "revision": "32429365a66933cb0137",
+    "url": "/test_page/static/js/2.90a71843.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test_page/static/js/2.90a71843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a4a8b0b7406de00e23",
+    "url": "/test_page/static/js/main.179ea9ce.chunk.js"
+  },
+  {
+    "revision": "ca0139345e36293ffb33",
+    "url": "/test_page/static/js/runtime-main.ce27e4ad.js"
+  },
+  {
+    "revision": "748e26febb0e59207f7b9781b53b15f8",
+    "url": "/test_page/static/media/dot.748e26fe.svg"
+  },
+  {
+    "revision": "de2a9158253dfcb6396e997317ae6c40",
+    "url": "/test_page/static/media/photo.de2a9158.png"
+  }
+]);
